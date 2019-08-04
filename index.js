c72b889bb14cf5826c3ddedd496f39870b72bf3b
Trevahr Robertson <Trevahr@gmail.com>
2019-08-03 21:52:05 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,28 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(timeString){
+
+  let stringSplit = timeString.split(":")
+  let timeHour = stringSplit[0]
+
+ if (timeHour < 12) {
+   return  "Good Morning"; }
+    else if (timeHour < 18) {
+     return  "Good Afternoon"
+    }
+    else {
+    return  "Good Evening"
+    }
+    
+  }
+  
+  
+  function displayMessage(x){
+
+    document.getElementById("greeting").innerText = x;
+  }
+
+
