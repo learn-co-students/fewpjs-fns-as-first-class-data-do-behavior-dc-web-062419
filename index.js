da98794d26edc2cbd92e852a0c3689e1af587a0b
github username <github email address>
2019-08-05 08:46:34 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+// The greet function should take one argument, a String which specifies the 24-hour time in the format HH:MM.
+
+// If the time is earlier than 12pm, return "Good Morning".
+// If the time is between 12pm and 5pm, return "Good Afternoon".
+// If the time is later than 5pm, return "Good Evening".
+
+function greet(time) {
+  let timetime = parseInt(time, 10);
+  if (timetime < 12)  return "Good Morning";
+  if (timetime > 17) return "Good Evening";
+  return "Good Afternoon";
+}
+
+// The displayMessage function should take one argument, a String.
+// When the function runs it should update the text inside the #greeting node with the content of the first argument.
+// It does not return anything.
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
