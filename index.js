d9a45325413788e7c55f0b63a7a3038a24cd2af5
SkaMateria <skamateria@gmail.com>
2019-08-05 09:00:40 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  var timeHour = timeString.split(":")[0];
+  var time = parseInt(timeHour);
+  if (time < 12) {
+    return "Good Morning";
+  }
+  else if (time < 17) {
+    return "Good Afternoon";
+  }
+  else {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
