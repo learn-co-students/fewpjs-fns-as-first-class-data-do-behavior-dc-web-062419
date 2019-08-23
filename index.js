3aa64d9d289b00ee1552b5473c7cd34c6f7f4888
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-08-23 17:23:12 -0400

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+    let hours = parseInt(time.split(":")[0])
+    let message
+    if (hours < 13) {
+        message = "Good Morning"
+    } else if (hours < 18) {
+        message = "Good Afternoon"
+    } else {
+        message = "Good Evening"
+    }
+    return message
+}
+
+function displayMessage(message) {
+    let greeting = document.getElementById("greeting")
+    greeting.innerText = message
+
+}
